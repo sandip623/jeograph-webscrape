6a4d70fa2a74dfe6498c76c6e6038575125beb84
sandip623 <sandip.gurung@hotmail.com>
2024-03-01 00:09:35 +0000

    azure blobstore cnx added

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+import json 
 from jg_webscraper import WebScraper, webscrape_extension
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-keyword = "Data Scientist"
+keyword = "Pharmacist"
 base_url = "https://www.reed.co.uk"
-job_listings = WebScraper(base_url=base_url, keyword=keyword).scrape_jobs()
-for job in job_listings:
-    print(job)
+listings = WebScraper(base_url=base_url, keyword=keyword).scrape_jobs()
+# convert to json str obj for data ingestion
+listings_dto = json.dumps(listings)
+
+# Azure Storage account details
+account_name = 'jeographstore001'
+account_key = 'Teyi0JFeE4G/tInDqC5Ykv2A/nIokl5pUx1MyxKJ2STo0wCi/SrCn1iS/SRoEoOye+gZ8iUrJL9z+AStKYxQVQ=='
+container_name = 'listingsdtoblobstore'
+blob_name = 'joblistingsdto.json'
+
+# Authenticate to Azure Storage
+blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net", credential=account_key)
+
+# Get the container client
+container_client = blob_service_client.get_container_client(container_name)
+
+# Create a blob client and upload JSON data
+blob_client = container_client.get_blob_client(blob=blob_name)
+blob_client.upload_blob(listings_dto, overwrite=True)
+
+print(f"listings_dto obj uploaded to Azure Blob Storage: {blob_name}")
+
+# TODO: setup oop blob config and initialiation w/ getter and setter for config class, async threading (likely daily than hourly due to cost incurrings)
